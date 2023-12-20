e0d65120aeee48adecef94f22730ee155174bbc5
SERRET-LARMANDE Arnaud <arnaud.serret-larmande@aphp.fr>
2023-12-20 19:17:08 +0100

    adding akiki exploration script

---
+++ akiki/exploration_akiki.R
@@ -0,0 +1,7 @@
+dir_akiki <- "//Psl-s-dfs/nas/SPEPS/BIOSPIM/UF Biostatistique/akiki"
+dir_akiki2 <- "//Psl-s-dfs/nas/SPEPS/BIOSPIM/UF Biostatistique/akiki2/BIOSTATISTICIEN"
+
+load(file.path(dir_akiki, "akiki.RData"))
+load(file.path(dir_akiki2, "akiki_tb.RData"))
+View(akiki)
+View(akiki_tb)
